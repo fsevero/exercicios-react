b40ba79ccdc8476350190752f20c0c17dc3882db
Severo <severo.fabricio@gmail.com>
2018-04-10 20:43:36 -0300

    First simple component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from "react-dom"
 
+import Component from "./component";
+
 ReactDOM.render(
-  <h1>Hello from React</h1>,
+  <Component />,
   document.getElementById('app')
 )
